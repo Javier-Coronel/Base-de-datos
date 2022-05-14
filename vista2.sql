3e6449b6301f52ab26b4a5d6f57ff2694db52a2c
Javier-Coronel <90841478+Javier-Coronel@users.noreply.github.com>
2022-05-14 14:02:19 +0200

    Update vista2.sql

--- vista2.sql
+++ vista2.sql
@@ -1,17 +1,10 @@
-CREATE VIEW `view1` AS
-    SELECT 
-        PE.*
-    FROM
-        ProductoElaboracion PE,
-        Porcentages P,
-        AlimentoElaborado AE,
-        SeElaboraEn SEE,
-        PlantaElaboracion PE2,
-        LugarTrabajo LT
-    WHERE
-        PE.idProductoElaboracion = P.ProductoElaboracion
-            && AE.IDAlimento = P.AlimentoElaborado
-            && AE.IDAlimento = SEE.IDAlimento
-            && PE2.idPlantaElaboracion = SEE.idPlantaElaboracion
-            && PE2.idPlantaElaboracion = LT.idLugar
-            && LT.Pais LIKE 'Spain';
+create view `view2` as
+select LT.*
+from
+LugarTrabajo LT
+inner join PlantaElaboracion PE
+on LT.idLugar = PE.idPlantaElaboracion
+inner join Lote L2
+on L2.PlantaElaboracion=idPlantaElaboracion
+where Year(L2.FechaCaducidad)=2022
+group by LT.idLugar;
