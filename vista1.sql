7c023566fea9808d26b7713b6ab6c13d6c67d3bf
Javier-Coronel <90841478+Javier-Coronel@users.noreply.github.com>
2022-06-16 09:58:45 +0200

    Update vista1.sql

--- vista1.sql
+++ vista1.sql
@@ -1,5 +1,5 @@
 CREATE VIEW `view1` AS
-select idProductoElaboracion 
+select PE.idProductoElaboracion, PE.Nombre, PE.PrecioPorKilo 
 from ProductoElaboracion PE, Porcentages P, AlimentoElaborado AE, SeElaboraEn SEE, PlantaElaboracion PE2, LugarTrabajo LT
 where PE.idProductoElaboracion = P.ProductoElaboracion && AE.IDAlimento = P.AlimentoElaborado && AE.IDAlimento = SEE.IDAlimento && PE2.idPlantaElaboracion = SEE.idPlantaElaboracion && PE2.idPlantaElaboracion = LT.idLugar 
 && LT.Pais like 'Spain';
